183b1ff3faa1f29ed48686c074ddd9f009df6121
Pavanstonegrid@gamil.com <Pavanstonegrid@gamil.com>
2025-08-19 09:25:36 +0000

    added offers.java file

--- app.java
+++ app.java
@@ -1,3 +1,4 @@
 Hello world
 This is my first line of code in the git repository.
 This is my second line of code in the project
+This code lin was written by pawan from another vm by cloning the git repositorys using the ssh
